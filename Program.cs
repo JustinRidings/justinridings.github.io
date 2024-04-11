a003a21fa34e7f8bb7cf89a1484c97d7acb6ad0c
Justin Ridings <juriding@microsoft.com>
2024-04-11 07:19:58 -0700

    Edited Service Configuration for pre-rendering

--- Program.cs
+++ Program.cs
@@ -4,13 +4,28 @@ using Microsoft.FluentUI.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components.DesignTokens;
 using PersonalSite;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+public class Program
+{
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddFluentUIComponents();
-builder.Services.AddDesignTokens();
-builder.Services.AddDataGridEntityFrameworkAdapter();
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+        ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
-await builder.Build().RunAsync();
+        await builder.Build().RunAsync();
+    }
+
+    static void ConfigureServices(IServiceCollection services, string baseAddress)
+    {
+        services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+        services.AddFluentUIComponents();
+        services.AddDesignTokens();
+        services.AddDataGridEntityFrameworkAdapter();
+    }
+}
+
+
+
+
