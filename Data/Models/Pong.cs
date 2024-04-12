8b1cb3a4d36a888c365f1c829df4fa1b601e1e9e
Justin Ridings <juriding@microsoft.com>
2024-04-12 11:15:49 -0700

    Added 'Pong' game

---
+++ Data/Models/Pong.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonalSite.Data.Models
+{
+    public class BallPosition
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+    }
+
+    public enum Player
+    {
+        One,
+        Two
+    }
+}
