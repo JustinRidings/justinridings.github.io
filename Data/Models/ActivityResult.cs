68a7806cdccf9d03e047694fd82dbc664c561ac3
Justin Ridings <juriding@microsoft.com>
2024-04-11 23:30:18 -0700

    Added bored page

---
+++ Data/Models/ActivityResult.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalSite.Data.Models
+{
+    public class ActivityResult
+    {
+        public string Activity { get; set; } = default!;
+        public double Accessibility { get; set; }
+        public string Type { get; set; } = default!;
+        public int Participants { get; set; }
+        public double Price { get; set; }
+        public string? Link { get; set; }
+    }
+}
